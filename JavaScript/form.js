10574065a4867d78865f469499816bd45871edd8
Anna <anna.elisabet5@gmail.com>
2024-03-21 19:48:40 +0100

    add formulario

--- JavaScript/form.js
+++ JavaScript/form.js
@@ -1,16 +1,57 @@
-function addFormValidation(){
-    let inputs=document.getElementsByTagName('input');
-    for(let input of inputs){
-        input.addEventListener("onBlur", validateForm)
-    }
+function manageFormSubmission(){
+    let name = document.getElementById("name").value;
+    let surname = document.getElementById("surname").value;
+    let birthday = document.getElementById("birthday").value;
+    let nick = document.getElementById("nick").value;
+    let rows = document.getElementById("rows").value;
+    let columns = document.getElementById("columns").value;
+    let mines=document.getElementById("mines").value;
+    let password=document.getElementById("password").value;
+    toItemForStorage(name, surname, birthday, nick, rows, columns, mines, password);
+
 }
 
-function validateForm(event){
-    console.log("se esta validando")
-    setCustomValidity("Must be over 18")
+function toItemForStorage(name, surname, birthday, nick, rows, columns, mines, password){
+    let formData = {
+        name: name,
+        surname: surname,
+        birthday: birthday,
+        nick: nick,
+        rows: rows,
+        columns: columns,
+        mines: mines,
+        password: password
+    };
+    window.localStorage.setItem("user", JSON.stringify(formData));//se guarda tambien en esta ventana para poder accedes si se quieren modificar algunos datos
+    window.opener.saveToLocalStorage("user", JSON.stringify(formData));//se envia el conjuto de datos a la ventana inicial
+    console.log("Se intenta cerrar la ventana")
+    window.close()//se cierra la ventana actual
 }
 
 function init(){
-    addFormValidation()
-    console.log("se ha cargado")
+    //rellenar y bloquear campos name, surname birthday nick y password si ya estan llenos
+        let nameField = document.getElementById("name");
+        let surnameField = document.getElementById("surname");
+        let birthdayField = document.getElementById("birthday");
+        let nickField = document.getElementById("nick");
+        let passwordField = document.getElementById("password");
+    
+        if (window.localStorage.getItem("user") !== null) {
+            let formData = JSON.parse(window.localStorage.getItem("user"));
+            nameField.value = formData.name;
+            surnameField.value = formData.surname;
+            birthdayField.value = formData.birthday;
+            nickField.value = formData.nick;
+            passwordField.value = formData.password;
+    
+            nameField.disabled = true;
+            surnameField.disabled = true;
+            birthdayField.disabled = true;
+            nickField.disabled = true;
+            passwordField.disabled = true;
+        }
+    
+    let button=document.getElementById("buttonSubmit")
+    button.addEventListener("click", manageFormSubmission);
+
 }
--- JavaScript/index.js
+++ JavaScript/index.js
@@ -5,10 +5,18 @@ let casillasPorRevelar = 0
 
 /***************     FUNCIONES     *******************/
 //TODO: hacer que casilla tenga el ATR de posicion y mina(? y que no se le tenga que pasar tablero.
-//TODO:prueba para ver si funciona :)
+//TODO:haecr popup
+//TODO: hacer que el juego acabe (WIN and LOSE)
+//añadir opcion de modificar los datos
+//hacer que no se puedan añadir banderas a casillas reveladas
+
 function pintaTablero(tablero) {
-  let contenedor = document.getElementById("tablero")
-  contenedor.classList.add("center");
+  //TODO:crear tablero si no existe o recuperar elemento tablero 
+  let tableroDOM =getOrCreateElement("tablero");
+    let contenedor = document.getElementById("contenedorJuego");
+    contenedor.appendChild(tableroDOM);
+    tableroDOM.classList.add("center");
+ 
   //recorrer tablero
   for (let fila of tablero.matrizCasillas) {
     //se crea fila
@@ -16,13 +24,23 @@ function pintaTablero(tablero) {
     filaFisica.classList.add("fila")
     //se crean casillas
     for (let elemento of fila) {
-      let casilla = configurarCasilla(elemento, contenedor, tablero)//se configura el texto, si tiene bamdera, el css,...
+      let casilla = configurarCasilla(elemento, tableroDOM, tablero)//se configura el texto, si tiene bamdera, el css,...
       filaFisica.appendChild(casilla);
     }
-    contenedor.appendChild(filaFisica);
+    tableroDOM.appendChild(filaFisica);
   }
   addContador(tablero)
+}
 
+function getOrCreateElement(id){
+  let elemento = document.getElementById(id);
+  if (!elemento) {
+    elemento = document.createElement('div');
+    elemento.id = 'tablero';
+  } else {
+    elemento.innerHTML = ''; 
+  }
+  return elemento
 }
 
 function configurarCasilla(elemento, contenedor, tablero) {
@@ -59,24 +77,24 @@ function addEmojiBandera(casilla) {
 }
 
 function eventoClickBandera(ev, contenedor, tablero, x, y) {
-  try{
+  try {
     ev.preventDefault();
-  tablero.toggleFlag(y, x)
-  console.log("click bandera")
-  contenedor.innerHTML = ""
-  pintaTablero(tablero)
-  }catch (error){
+    tablero.toggleFlag(y, x)
+    console.log("click bandera")
+    contenedor.innerHTML = ""
+    pintaTablero(tablero)
+  } catch (error) {
     console.log("Error: ", error.message)
     addErrorMessage("No flags left 😔")
   }
-  
+
 }
 
 function eventoClickRevelar(contenedor, tablero, x, y) {
   try {
     tablero.revelarCasilla(y, x)
     console.log("click revelar")
-    if(banderasColocadas==casillasPorRevelar) gameOverDOM()
+    if (banderasColocadas == casillasPorRevelar) gameOverDOM()
     contenedor.innerHTML = ""
     pintaTablero(tablero)
   } catch (error) {
@@ -108,11 +126,11 @@ function removeErrorMessage() {
 }
 
 function gameOverDOM() {
-   let i = 10;
+  let i = 10;
   const idInterval = setInterval(() => {
     removeErrorMessage();
-    let message=`¡BOOM! <br> Game will restart in ${i--} seconds`
-    if(banderasColocadas==casillasPorRevelar) message=`YOU WON! <br> Game will restart in ${i--} seconds`
+    let message = `¡BOOM! <br> Game will restart in ${i--} seconds`
+    if (banderasColocadas == casillasPorRevelar) message = `YOU WON! <br> Game will restart in ${i--} seconds`
     addErrorMessage(message);
     if (i === 0) {
       clearInterval(idInterval);
@@ -122,11 +140,42 @@ function gameOverDOM() {
   removeErrorMessage()
 }
 
+/* Funcion que es llamada desde form.js para añadir los datos al localStorage*/
+function saveToLocalStorage(key, value) {
+  window.localStorage.setItem(key, value);
+}
+
+/*Metodo que accede al local storage para recuperar la informacion del usuario, si no hay informacion, se abre el formulario*/
+function getLocalStorage(modifyData) {
+  if (!modifyData){
+    addErrorMessage("You have to fill the form before playing 🤓 ")
+    window.open("http://127.0.0.1:5500/html/form.html")
+
+  } else{
+     if (window.localStorage.getItem("user") == null) {
+    window.open("http://127.0.0.1:5500/html/form.html")
+  } else {
+    removeErrorMessage()
+  }
+  return window.localStorage.getItem("user")
+  }
+ 
+}
+
 function init() {
-  let nuevoTablero = new Tablero(3, 3, 1);
-  casillasPorRevelar==nuevoTablero.filas*nuevoTablero.columnas
-  pintaTablero(nuevoTablero);
-  
+  removeErrorMessage()
+  let info_usuario=" "
+  if(window.localStorage.getItem("user")==null) info_usuario = getLocalStorage(false)  
+  else info_usuario = window.localStorage.getItem("user")
+  removeErrorMessage()
+  if (info_usuario) {
+    info_usuario = JSON.parse(info_usuario);
+    let nuevoTablero = new Tablero(info_usuario.columns, info_usuario.rows, info_usuario.mines);
+    casillasPorRevelar == nuevoTablero.filas * nuevoTablero.columnas
+    pintaTablero(nuevoTablero);
+  }
+
+
 }
 
 
